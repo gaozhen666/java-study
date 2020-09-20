5756640315f4f8528c8ef63dc7db7b408fbb315b
gaozhen <1154873891@qq.com>
2020-09-20 17:28:12 +0800

    第一次上传

---
+++ Calendar/src/com/company/TestCalendar.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+public class TestCalendar {
+    public static void main(String[] args) throws ParseException {
+        System.out.println("请输入日期（格式：2020-9-10）：");
+        Scanner scanner=new Scanner(System.in);
+        String str=scanner.nextLine();
+
+        DateFormat df=new SimpleDateFormat("yyyy-MM-dd");
+        Date date=df.parse(str);//字符串转化为时间对象
+
+        Calendar c=new GregorianCalendar();
+        c.setTime(date);//时间对象转化为日期对象
+
+        int days=c.getActualMaximum(Calendar.DATE);
+        int day=c.get(Calendar.DAY_OF_MONTH);
+
+        System.out.println("日\t一\t二\t三\t四\t五\t六");
+
+        c.set(Calendar.DAY_OF_MONTH,1);
+        for (int i=0;i<c.get(Calendar.DAY_OF_WEEK)-1;i++){
+            System.out.print("\t");
+        }
+
+
+        for (int i=1;i<=days;i++){
+
+            if (day==c.get(Calendar.DAY_OF_MONTH)){
+                System.out.print(c.get(Calendar.DAY_OF_MONTH)+"#\t");
+            }else{
+                System.out.print(c.get(Calendar.DAY_OF_MONTH)+"\t");
+            }
+
+            if(c.get(Calendar.DAY_OF_WEEK)==Calendar.SATURDAY){
+                System.out.println();
+            }
+            c.add(Calendar.DAY_OF_MONTH,1);
+        }
+    }
+}
